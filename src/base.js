6dbbdb3d61ab4be70d9153ef604e82abade1861c
Vinicius Freitas <viniciusfreitasrj17@gmail.com>
2019-10-21 23:09:59 -0300

    Server JSON with Express

--- src/base.js
+++ src/base.js
@@ -1,3 +1,7 @@
+const fs = require('fs')
+let db = require('../db/db.json')
+
+/*
 const sequence = {
 	_id: 1,
 	get id(){
@@ -6,24 +10,36 @@ const sequence = {
 }
 
 produtos = {}
+*/
+function fsUp(b){
+	fs.writeFile(__dirname + '/../db/db.json', JSON.stringify(b), 'utf8', (err) => {
+		console.log(err || 'Save data')
+	})
+}
+const getProdutos = () => db
 
-const getProdutos = () => Object.values(produtos) || {}
+const getProduto = (id) => db[id] || {}
 
-const getProduto = (id) => produtos[id] || {}
+const setProduto = (prod) => {
+	let i = (Object.keys(db).length ? Object.keys(db) : [0]).reduce((ult, num) => { return (ult > num) ? ult : num })
+	if(!prod.id)  prod.id = ++i
+	db[prod.id] = prod
 
-const setProduto = (produto) => {
-	const prod = produto
-	if(!prod.id)  prod.id = sequence.id
-	produtos[prod.id] = produto
+	fsUp(db)
 	return prod
 }
 
 const delProduto = (id) => {
-	const prod = produtos[id]
-	delete produtos[id]
+	const prod = db[id]
+	delete db[id]
+	fsUp(db)
 	return prod
 }
 
-const delProdutos = () => produtos = {}
+const delProdutos = () => {
+	db = {}
+	fsUp(db)
+	return db
+}
 
 module.exports = { getProdutos, getProduto, setProduto, delProduto, delProdutos }
